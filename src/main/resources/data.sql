0ce70712219a30c58c58b2e628de2572603e4cd9
brandon <dmdjqn@gmail.com>
2022-05-22 21:06:44 +0900

    feat: JPA 의존성추가 및 셋업

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user values(1,sysdate(),'USER1', 'test1111', '920113-1039281');
+insert into user values(2,sysdate(),'USER2', 'test1111', '720113-1039281');
+insert into user values(3,sysdate(),'USER3', 'test1111', '320113-1039281');
+insert into user values(4,sysdate(),'USER4', 'test1111', '520113-1039281');
+insert into user values(5,sysdate(),'USER5', 'test1111', '020113-1039281');
+insert into user values(6,sysdate(),'USER6', 'test1111', '220113-1039281');
